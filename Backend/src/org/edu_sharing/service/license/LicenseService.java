29541f91f91284398d8685c84be13bd704d9b5be
Edmond Kacaj <edmondikacaj@gmail.com>
2021-11-19 09:02:55 +0100

    roll back last commit

--- Backend/src/org/edu_sharing/service/license/LicenseService.java
+++ Backend/src/org/edu_sharing/service/license/LicenseService.java
@@ -71,7 +71,7 @@ public class LicenseService {
 			}
 
 			if(result.contains("${locale}")){
-				result = result.replace("${locale}", country.toLowerCase());
+				result = result.replace("${locale}", country);
 			}
 		}
 		
